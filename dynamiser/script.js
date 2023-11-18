7d832aad04fe3da3e96a5bf1b8c8e2394cfe6dfb
NathalieDrnt <nathaliedarnaudat@outlook.com>
2023-11-18 19:42:44 +0100

    add script_et_récup

---
+++ dynamiser/script.js
@@ -0,0 +1,6 @@
+//Récupération de mes éléments HTML
+let form = document.querySelector("form");
+let firstName = document.getElementById("first-name");
+let lastName = document.getElementById("last-name");
+let message = document.getElementById("message");
+let errorMessage = document.getElementById("error-message");
